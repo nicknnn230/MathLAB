bd27daecef8359dba0f28bb65a7ecdf3b4ddf683
nicknnn230 <merseberger2@gmail.com>
2024-11-03 23:25:41 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,139 @@
+import tkinter as tk
+from sympy import symbols, diff, integrate, sympify, SympifyError, lambdify, limit, solve, simplify, Subs
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+x = symbols('x')
+
+def ableiten():
+    global diff_label
+
+
+    function = e1.get()
+    try:
+
+        expression_diff = sympify(function)
+
+        differentiation = diff(expression_diff, x)
+        simplify(differentiation)
+    except SympifyError:
+        differentiation = "Ungültiger Ausdruck. Bitte gültige Funktion eingeben."
+
+
+    if diff_label is None:
+
+        diff_label = tk.Label(root, text=f"1. Ableitung von f(x): {differentiation}")
+        diff_label.grid(row=1, column=0, columnspan=2, sticky="w")
+    else:
+
+        diff_label.config(text=f"1. Ableitung von f(x): {differentiation}")
+
+
+
+
+def integrieren():
+    global integrate_label
+
+    
+
+    function = e1.get()
+    try:
+
+        expression_inte = sympify(function)
+
+        integration = integrate(expression_inte, x)
+        simplify(integration)
+    except SympifyError:
+        integration = "Ungültiger Ausdruck. Bitte gültige Funktion eingeben."
+
+    if integration != "Ungültiger Ausdruck. Bitte gültige Funktion eingeben.":
+        if integrate_label is None:
+
+            integrate_label = tk.Label(root, text=f"Stammfunktion von f(x): {integration} + c")
+            integrate_label.grid(row=2, column=0, columnspan=2, sticky="w")
+        else:
+
+            integrate_label.config(text=f"Stammfunktion von f(x): {integration} + c")
+    else:
+        if integrate_label is None:
+
+            integrate_label = tk.Label(root, text=f"Stammfunktion von f(x): {integration}")
+            integrate_label.grid(row=2, column=0, columnspan=2, sticky="w")
+        else:
+
+            integrate_label.config(text=f"Stammfunktion von f(x): {integration}")
+
+def Nullstellen():
+    global solve_label
+    function = e1.get()
+    try:
+
+        expression_solve = sympify(function)
+
+        nullstellen = solve(expression_solve, x)
+
+    except SympifyError:
+        nullstellen = "Ungültiger Ausdruck. Bitte gültige Funktion eingeben."
+
+
+    if solve_label is None:
+
+        solve_label = tk.Label(root, text=f"Nullstellen von f(x): {nullstellen}")
+        solve_label.grid(row=4, column=0, columnspan=2, sticky="w")
+    else:
+
+        solve_label.config(text=f"Nullstellen von f(x): {nullstellen}")   
+
+
+
+def plotten():
+    function = e1.get()
+
+    f_lambda = lambdify(x, function, 'numpy')
+
+    x_vals = np.linspace(-50, 50, 500)
+    y_vals = f_lambda(x_vals)
+
+
+    plt.plot(x_vals, y_vals, label = str(function), color='blue')
+    plt.xlabel('x')
+    plt.ylabel('f(x)')
+    plt.title('Graph der Funktion f(x)')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+
+
+root = tk.Tk()
+root.geometry('900x700')
+root.title('MathLAB')
+
+
+menu = tk.Menu(root)
+
+algebra_menu = tk.Menu(menu, tearoff=0)
+algebra_menu.add_command(label='Ableiten', command=ableiten)
+diff_label = None
+algebra_menu.add_command(label='Stammfunktion', command=integrieren)
+integrate_label = None
+algebra_menu.add_command(label='Nullstellen', command=Nullstellen)
+solve_label = None
+menu.add_cascade(label='Algebra', menu=algebra_menu)
+
+plot_menu = tk.Menu(menu, tearoff=0)
+plot_menu.add_command(label='Plot', command=plotten)
+menu.add_cascade(menu=plot_menu, label='Plotten')
+
+
+tk.Label(root, text="f(x) =").grid(row=0, column=0, sticky="w")
+e1 = tk.Entry(root)
+e1.grid(row=0, column=1, sticky="we", padx=5, pady=5)
+
+root.grid_columnconfigure(1, weight=1)
+
+root.config(menu=menu)
+
+
+root.mainloop()
